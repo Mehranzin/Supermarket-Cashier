d74c3ba9f77a5a03c2e8ce5abcf5674bd7a5f609
Mehran <mehranmesrob@gmail.com>
2025-07-26 15:58:00 -0300

    Atualização automatica no python

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
 from flask import Flask, render_template
+from livereload import Server
 
 app = Flask(__name__)
 
 produtos = [
     {"id": 1, "nome": "Arroz", "preco": 7.0, "estoque": 10},
-    {"id": 1, "nome": "Feijão", "preco": 5.50, "estoque": 7},
-    {"id": 1, "nome": "Pão do Sírio", "preco": 6.0, "estoque": 2},
+    {"id": 2, "nome": "Feijão", "preco": 5.50, "estoque": 7},
+    {"id": 3, "nome": "Pão do Sírio", "preco": 6.0, "estoque": 2},
 ]
 
 @app.route("/")
 def index():
     return render_template("index.html", produtos=produtos)
 
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    server = Server(app.wsgi_app)
+    server.serve(debug=True)
